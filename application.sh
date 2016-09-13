8ade7e07e2d5176bd84c9e70c73372f8ee559966
armstrongmsg <armstrongmsg@gmail.com>
2016-09-13 10:38:38 -0300

    Adding application used in the experiment

---
+++ application.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FACTORIAL_N=$1
+PROGRESS_FILE="job.progress"
+
+function factorial()
+  if (( $1 < 2 ))
+  then
+    echo 1
+  else
+    echo "$1 * $(factorial $(( $1 - 1 )))" | bc
+  fi
+
+echo "0" > $PROGRESS_FILE
+
+for i in `seq 1 100` ;
+do 
+	factorial $FACTORIAL_N > /dev/null
+	echo $i >> $PROGRESS_FILE
+done
